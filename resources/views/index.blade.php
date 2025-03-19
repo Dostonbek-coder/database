a99861a9022a454cbd516a76af0882fe96096e8f
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-19 08:47:48 +0500

    Blade file created and form written

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')    
+    <h1>Products</h1>
+    <a href="{{ route('products.create') }}">Create Product</a>
+
+    @foreach($products as $product)
+        <div>
+            <h2>{{ $product->title }}</h2>
+            <p>{{ $product->description }}</p>
+            <p>Price: ${{ $product->price }}</p>
+            <p>Discount: {{ $product->discount }}%</p>
+            
+            <a href="{{ route('products.show', $product) }}">View</a>
+            <a href="{{ route('products.edit', $product) }}">Edit</a>
+            
+            <form action="{{ route('products.destroy', $product) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete</button>
+            </form>
+        </div>
+    @endforeach
+@endsection
